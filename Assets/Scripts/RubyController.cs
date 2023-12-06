7b515c88b60ff142eda427c0d35ef537d648584c
Jamarig7 <147067709+Jamarig7@users.noreply.github.com>
2023-12-06 16:36:42 -0500

    part 7

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,6 +14,13 @@ public class RubyController : MonoBehaviour
 
     public float speed = 5;
 
+    float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,8 @@ public class RubyController : MonoBehaviour
 
 
       currentHealth = maxHealth;
+
+      animator = GetComponent<Animator>();
       
     }
     
@@ -34,6 +43,25 @@ public class RubyController : MonoBehaviour
     { 
        horizontal  =  Input.GetAxis("Horizontal");
        vertical  =  Input.GetAxis("Vertical");
+
+       Vector2 move = new Vector2(horizontal, vertical);
+
+       if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+       if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -49,6 +77,14 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+      if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
